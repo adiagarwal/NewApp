015fc8aa56082419e2a1ada8796479a3ca2d48fe
unknown <aditya20322@gmail.com>
2022-12-24 21:22:56 +0530

    AdditionalChanges

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
   <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">News Today</a>
+    <a className="navbar-brand" href="/">News Keeda</a>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
